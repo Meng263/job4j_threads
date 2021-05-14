3f401ec61a4f3f5ce8446a35ccc5b57c8edeff6b
Oleg Sivakov <Meng@siams.com>
2021-05-14 23:30:45 +0500

    Initial commit

---
+++ src/main/java/ru/job4j/multithreading/thread/ConcurrentOutput.java
@@ -0,0 +1,11 @@
+package ru.job4j.multithreading.thread;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
---
+++ src/test/java/ru/job4j/multithreading/thread/ConcurrentOutputTest.java
@@ -0,0 +1,14 @@
+package ru.job4j.multithreading.thread;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class ConcurrentOutputTest {
+    @Test
+    public void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
