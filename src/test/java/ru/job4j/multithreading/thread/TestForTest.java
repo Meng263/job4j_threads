e7ef73fd403ed0bde31833233f5d91b74ccd402e
Oleg Sivakov <Meng@siams.com>
2021-05-16 00:03:20 +0500

    fix ConcurrentOutput

--- src/test/java/ru/job4j/multithreading/thread/TestForTest.java
+++ src/test/java/ru/job4j/multithreading/thread/TestForTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-class ConcurrentOutputTest {
+class TestForTest {
     @Test
     public void simpleTest() {
         assertEquals(2, 1 + 1);
