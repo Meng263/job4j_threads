4f10cdb670256e9b18ae9e8a87df54db55915352
Oleg Sivakov <Meng@siams.com>
2021-06-13 22:27:05 +0500

    fix by review

--- src/main/java/ru/job4j/multithreading/thread_pools/simple/ThreadPool.java
+++ src/main/java/ru/job4j/multithreading/thread_pools/simple/ThreadPool.java
@@ -6,14 +6,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ThreadPool {
-    int size = Runtime.getRuntime().availableProcessors();
+    private final int size = Runtime.getRuntime().availableProcessors();
     private final List<Thread> threads = new LinkedList<>();
     private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size);
-    private Boolean stopFlag = false;
 
-    ThreadPool() {
+    public ThreadPool() {
         for (int i = 0; i < size; i++) {
-            threads.add(new Runner(tasks, stopFlag));
+            threads.add(new Runner(tasks));
         }
         threads.forEach(Thread::start);
     }
@@ -23,23 +22,20 @@ public class ThreadPool {
     }
 
     public void shutdown() {
-        stopFlag = true;
         threads.forEach(Thread::interrupt);
     }
 
     private static class Runner extends Thread {
         private final SimpleBlockingQueue<Runnable> tasks;
-        private volatile Boolean stopFlag;
 
-        private Runner(SimpleBlockingQueue<Runnable> tasks, Boolean stopFlag) {
+        private Runner(SimpleBlockingQueue<Runnable> tasks) {
             this.tasks = tasks;
-            this.stopFlag = stopFlag;
         }
 
         @Override
         public void run() {
             Runnable job;
-            while (!stopFlag && !Thread.currentThread().isInterrupted()) {
+            while (!Thread.currentThread().isInterrupted()) {
                 if ((job = tasks.poll()) != null) {
                     job.run();
                 }
